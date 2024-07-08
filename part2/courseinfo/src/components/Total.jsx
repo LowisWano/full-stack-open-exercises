9ac9a92411158fde9f04a07f24eb663d3a9f226c
Luis <110468848+LowisWano@users.noreply.github.com>
2024-07-09 00:21:12 +0800

    finished part-2 courseinfo

---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,13 @@
+const Total = ({ parts }) =>{
+    return (
+      <div>
+        <strong>
+            total of {parts.reduce((sum, part)=>{
+                return sum + part.exercises;
+            }, 0)} exercises
+        </strong>
+      </div>
+    )
+  }
+
+export default Total;
