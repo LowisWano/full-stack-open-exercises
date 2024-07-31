5763eba759072d77caa0930c78b2059ae3dd0bcf
wiswiss <luisandreiouano@gmail.com>
2024-07-31 11:40:38 +0800

    feat: exercise 4.10

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -11,15 +11,13 @@ const test_blogs = [
     "title": "Oi Hughie, Homelander done killed me wife and took me bloody son",
     "author": "Butcher",
     "url": "https://www.youtube.com/watch?v=PmHlYDAxSOM",
-    "likes": 0,
-    "id": "66a8fa8d817a30986683aa67"
+    "likes": 0
   },
   {
     "title": "Flash is fast flash is cool",
     "author": "Soldier Boy",
     "url": "https://www.youtube.com/watch?v=li2WDMgHxD0",
-    "likes": 0,
-    "id": "66a9001cf13016add0fd2167"
+    "likes": 0
   }
 ]
 
@@ -47,6 +45,25 @@ describe('GET requests to API', () => {
     const response = await api.get('/api/blogs')
     assert(response.body[0].hasOwnProperty("id"))
     assert(!response.body[0].hasOwnProperty("_id"))
+  })
+
+  test('a valid blog can be added', async () => {
+    const new_blog = {
+      'title': 'Ur ur ur ur ur',
+      'author': 'Freddy Fazbear',
+      'url': 'https://www.youtube.com/watch?v=PPSzurNBr8s',
+      'likes': 0
+    }
+
+    await api.post('/api/blogs')
+      .send(new_blog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+      const response = await api.get('/api/blogs')
+      const titles = response.body.map(r => r.title)
+      assert.strictEqual(response.body.length, test_blogs.length + 1)
+      assert(titles.includes(new_blog.title))
   })
 
 })
