9e39feaa64f8c4768501be18b0b9718eb3f30814
wiswiss <luisandreiouano@gmail.com>
2024-08-08 15:45:32 +0800

    feat: exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote  } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    return state.anecdotes.filter((anecdote)=>anecdote.content.toLowerCase().includes(state.filter))
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
